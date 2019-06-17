2c9316bc8cc732b837c26546e50a5878f333efb4
sara shelton <saranic314@gmail.com>
2019-06-17 14:46:55 -0500

    Changed switch to off

---
+++ practice-git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice-git
+//
+//  Created by sara shelton on 6/17/19.
+//  Copyright © 2019 sara shelton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
